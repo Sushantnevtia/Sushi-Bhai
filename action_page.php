1e21575dcad0630c5e48e2220eeeb66443a8ff7c
Aimer <92011132+Sushantnevtia@users.noreply.github.com>
2023-07-30 22:50:30 +0530

    Add files via upload

---
+++ action_page.php
@@ -0,0 +1,9 @@
+<?php
+if(isset($_POST['submit_form'])){
+	$firstname=$_POST['firstname']
+	$lastname=$_POST['lastname']
+	$email=$_POST['email']
+	echo $subject=$_POST['subject']
+	$FromName="Sushi Bhai"
+}
+?>
